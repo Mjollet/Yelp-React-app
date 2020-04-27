da17e0cced3312adc950b65e18d4c8f1fea39b82
MJollet <48050378+Mjollet@users.noreply.github.com>
2020-04-27 19:10:40 +0200

    restaurant details displayed

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,27 +1,54 @@
 import React from 'react' ;
 import {View, Text, StyleSheet, FlatList} from 'react-native';
 import ResultsDetail from '../components/ResultsDetail';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { withNavigation } from 'react-navigation';
 
-const ResultsList = ({ title, results }) => {
-    return <View>
+const ResultsList = ({ title, results, navigation }) => {
+   if (!results.length){
+       return null;
+   }
+   
+   
+    return (
+    <View style= {styles.container}>
         <Text style= {styles.title}>{title}</Text>
-        <Text> Results: {results.length}</Text>
+        <Text style= {styles.total}> Results: {results.length}</Text>
         <FlatList 
         horizontal
+        showsHorizontalScrollIndicator={false}
         data={results}
         keyExtractor={(result) => result.id}
         renderItem={({ item }) => {
-        return <ResultsDetail result={item} />
+            return (
+               <TouchableOpacity onPress={()=> navigation.navigate('ResultsShow', { id: item.id })}> 
+                <ResultsDetail result={item} />
+               </TouchableOpacity>
+            )
         }}
         />
+
     </View>
+    )
 };
 
 const styles = StyleSheet.create({
+    container: {
+        marginBottom: 10
+    },
+
+    total: {
+        fontSize: 13,
+        marginLeft: 15,
+        marginBottom: 5
+    },
+
     title: {
         fontSize: 18,
         fontWeight: 'bold',
+        marginLeft: 15,
+        marginBottom: 5
     }
 });
 
-export default ResultsList;
+export default withNavigation(ResultsList);
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
@@ -7,6 +7,8 @@ import ResultsList from '../components/ResultsList';
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
+
+    console.log(results)
 
     const filterResultsByPrice = (price) => {
         //price ==='€' || '€€' || '€€€'
@@ -16,30 +18,31 @@ const SearchScreen = () => {
     };
 
     return (
-        <View>
+        <>
             <SearchBar 
                 term={term} 
                 onTermChange={setTerm} 
                 onTermSubmit={() => searchApi(term)}
             />
-            <Text>You look for { term } ! </Text>
-                {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found { results.length } results </Text>
-
+        <ScrollView>
             <ResultsList 
             results={filterResultsByPrice('€')}
             title="Cost Effective"
+ 
             />
             <ResultsList 
             results={filterResultsByPrice('€€')}
             title="Big Pricier"
+
             />
             <ResultsList 
             results={filterResultsByPrice('€€€')}
             title="Big Spender"
-            />
 
-        </View>
+            />
+        </ScrollView>
+        </>
     );
 };
 
